3b1887e2ec937297217d05ed085c393f7e6c1713
Volodymyr <kryvkovr@gmail.com>
2021-11-01 11:13:32 +0200

    raise version to 7

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
       {/* <Paper style={{ height: "100vh" }}> */}
 
         <Grid container direction="column">
-          <Typography variant="h1">Version 5</Typography>
+          <Typography variant="h1">Version 7</Typography>
         </Grid>
 
         <Button variant="contained" color="primary">
