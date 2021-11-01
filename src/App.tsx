e99e42d3226eb19f6bbea4c3b9e6133a17dfc52f
Volodymyr <kryvkovr@gmail.com>
2021-11-01 11:09:18 +0200

    add browser detecking

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
 import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { Grid, Typography, Button, Paper } from '@mui/material';
+import { Grid, Typography, Button, Paper, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    // primary?: PaletteColorOptions;
+    // secondary?: PaletteColorOptions;
+    // error?: PaletteColorOptions;
+    // warning?: PaletteColorOptions;
+    // info?: PaletteColorOptions;
+    // success?: PaletteColorOptions;
+    // mode?: PaletteMode;
+    // tonalOffset?: PaletteTonalOffset;
+    // contrastThreshold?: number;
+    // common?: Partial<CommonColors>;
+    // grey?: ColorPartial;
+    // text?: Partial<TypeText>;
+    // divider?: string;
+    // action?: Partial<TypeAction>;
+    // background?: Partial<TypeBackground>;
+
+
+
+    // primary: {
+    //   main: '#dd5',
+    // },
+    // secondary: {
+    //   main: '#66c',
+    // },
+  },
+});
 
 function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    // Обновляем заголовок документа с помощью API браузера
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      alert('Your browser is ....  please turn off DARK MODE');
+    console.log(window.navigator);
+    console.log(window.navigator.userAgent);
+    const isSamsungBrowser = navigator.userAgent.indexOf('SamsungBrowser') > - 1;
+    if(isSamsungBrowser){
+      alert('Samsung browser')
     }
+    const isMiBrowser = navigator.userAgent.indexOf('MiuiBrowser') > - 1;
+    if(isMiBrowser){
+      alert('MI browser')
+    }
+   
   }, []);
 
 
   return (
-    // <Paper style={{ height: "100vh"}}>
-    <div>
-      <Grid container direction="column">
-        <Typography variant="h1">Version 5</Typography>
-      </Grid>
+    <ThemeProvider theme={theme}>
+      {/* <div className='cont'>
+        lalalal
+      </div> */}
+      {/* <Paper style={{ height: "100vh" }}> */}
 
-      <Button variant="contained" color="primary">
-        First button
-      </Button>
+        <Grid container direction="column">
+          <Typography variant="h1">Version 5</Typography>
+        </Grid>
 
-      <Button variant="contained" color="secondary">
-        Second
-      </Button>
+        <Button variant="contained" color="primary">
+          First button
+        </Button>
+        <Button variant="contained" color="primary">
+          First button
+        </Button>
 
-    </div >
+
+        <Button variant="contained" color="secondary">
+          Second
+        </Button>
+      {/* </Paper> */}
+    </ThemeProvider>
   )
 }
 
